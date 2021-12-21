dc1c321d577d6ddd069d73a26babccef21ad6e3a
Bagas <bagaskara822@gmail.com>
2021-12-21 23:35:51 +0700

    Add project files.

---
+++ palindrome/palindrome.cpp
@@ -0,0 +1,26 @@
+﻿// palindrome.cpp : Checks whether a string is a palindrome or not.
+//
+
+#include "palindrome.h"
+#include <string>
+
+std::string strrev(std::string str) { // reverse the provided string for palindrome check later
+	if (str.empty()) return "";
+
+	std::string revstr;
+	for (int i = str.length() - 1; i >= 0; i--)
+		revstr += str.at(i);
+	return revstr;
+}
+
+int palindrome_check(std::string str) { // checks for palindrome string
+	return str == strrev(str);
+}
+
+int main()
+{
+	std::string a;
+	std::getline(std::cin, a);
+	std::cout << strrev(a) << palindrome_check(a);
+	return 0;
+}
